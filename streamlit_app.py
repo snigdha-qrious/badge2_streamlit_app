9627fadb8bb9a4914544db723322e98452bfd06a
snigdha-qrious <150637560+snigdha-qrious@users.noreply.github.com>
2023-11-27 15:08:02 +1300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 select_fruits = st.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
 fruits_to_show = my_fruit_list.loc[select_fruits ]
 
-st.dataframe(my_fruit_list)
+st.dataframe(fruits_to_show)
 
 st.title('My parents new healthy diner')
 
