48675b7ded6d476fb8d9b394c15d7dbb2aaf7bb8
snigdha-qrious <150637560+snigdha-qrious@users.noreply.github.com>
2023-11-27 14:56:43 +1300

    Adding dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,8 @@
 import streamlit as st
+import pandas 
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+st.dataframe(my_fruit_list)
 
 st.title('My parents new healthy diner')
 
